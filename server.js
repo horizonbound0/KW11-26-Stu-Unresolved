141f0b2bc1ef765a4cba7e51d3550afeca395b6b
Kenneth A Wagner <kenw2190@gmail.com>
2024-07-27 15:18:17 -0500

    last commit

--- server.js
+++ server.js
@@ -12,4 +12,4 @@ app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`App listening on port http://localhost:${PORT}`));
